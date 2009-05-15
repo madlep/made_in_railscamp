f5d271be8f713af50b13500a00cea131580f5717
madlep <madlep@ubercharged.net>
2009-05-15 23:51:06 +1000

    Initial version

---
+++ init.rb
@@ -0,0 +1,2 @@
+# Include hook code here
+require 'made_in_railscamp'
---
+++ lib/made_in_railscamp.rb
@@ -0,0 +1,27 @@
+module Madlep
+  class MadeInRailscamp
+    def initialize(app, message = "<p>I made this at Railscamp after drinking beer and not doing productive stuff</p>")
+      @app = app
+      @message = message
+    end
+
+    def call(env)
+      return @app.call(env) unless %w[GET].include?(env['REQUEST_METHOD'])
+
+      status, headers, body = @app.call(env)
+      puts headers.inspect
+      body.body.gsub!(/<\/html>/, "#{@message}</html>")
+      
+      content_length = headers['Content-Length']
+      
+      if content_length
+        length = content_length.to_i
+        length -= "</html>".length
+        length += @message.length
+        headers['Content-Length'] = length.to_s
+      end
+      
+      [status, headers, body]
+    end
+  end
+end
