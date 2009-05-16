4c3f81a94052dc5d47f22c5b0bb63ea812918628
madlep <madlep@ubercharged.net>
2009-05-16 11:40:05 +1000

    Cleaned up and made it more 'correct' with rspec

--- lib/made_in_railscamp.rb
+++ lib/made_in_railscamp.rb
@@ -1,3 +1,5 @@
+require 'rack/utils'
+
 module Madlep
   class MadeInRailscamp
     def initialize(app, message = "<p>I made this at Railscamp after drinking beer and not doing productive stuff</p>")
@@ -7,20 +9,30 @@ module Madlep
 
     def call(env)
       return @app.call(env) unless %w[GET].include?(env['REQUEST_METHOD'])
-
+      
       status, headers, body = @app.call(env)
-      puts headers.inspect
-      body.body.gsub!(/<\/html>/, "#{@message}</html>")
       
-      content_length = headers['Content-Length']
+      new_body, new_headers = say_made_at_railscamp!(body, headers)
       
-      if content_length
-        length = content_length.to_i
-        length += @message.length
-        headers['Content-Length'] = length.to_s
+      [status, new_headers, new_body]
+    end
+    
+    private
+    def say_made_at_railscamp!(body, headers)
+
+      new_body = []
+      new_content_length = 0
+      body.each do |body_part|
+        new_body_part = body_part.gsub(/<\/body>/, "#{@message}</body>")
+        new_body << new_body_part
+        new_content_length += Rack::Utils.bytesize(new_body_part)
       end
       
-      [status, headers, body]
+      if headers['Content-Length']
+        headers['Content-Length'] = new_content_length.to_s
+      end
+      
+      [new_body, headers]
     end
   end
 end
